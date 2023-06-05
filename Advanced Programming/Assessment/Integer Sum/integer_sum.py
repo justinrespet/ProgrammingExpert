34dd71e1f6ce6d3d6d505a46273d3167c5aac319
Justin Respet <justinrespet@gmail.com>
2023-06-05 12:29:33 -0600

    Testing flatten

--- Advanced Programming/Assessment/Integer Sum/integer_sum.py
+++ Advanced Programming/Assessment/Integer Sum/integer_sum.py
@@ -26,11 +26,41 @@ or list
 """
 
 # sample data
-data1 = ('1', '2', -0.9, 4, [5, 'hi', '3'])
+test1 = ('1', '2', -0.9, 4, [5, 'hi', '3'])
+test2 = (2, 3, 4, -2, '2', ['1', '2', 3], 2.3)
+test3 = ()
+test4 = ('2.3', 'true', [1, 'hello', 4])
 
 
 # flatten lists decorator
+# - This is going to involve the splat operator, but I'm not certain how exactly so
+# I'm going to play around with it
+# - Could I aggregate all inputs into a list, and then send an unpacked version of
+# that to the function?
+# - from the instructions, I can assume that I will receive no iterable other than a list,
+# and that I will not receive nested lists
 def flatten_lists(func):
-    def wrapper(*args, **kwargs):
+    def wrapper(*args):
+        returnList = []
 
-        return
+        for item in args:
+            if isinstance(item, list):
+                for i in item:
+                    returnList.append(i)
+            else:
+                returnList.append(item)
+
+        return func(*returnList)
+    return wrapper
+
+
+# - as of being in integer_sum, my data will be 'flat' if they are individual elements
+# inside a tuple, as *args creates a tuple of any
+@flatten_lists
+def integer_sum(*args):
+    return args
+
+
+print("***")
+print(integer_sum(*data3))
+print("***")

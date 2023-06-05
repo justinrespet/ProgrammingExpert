1ecbf75688f36f8aadaf650890479a4198921ceb
Justin Respet <justinrespet@gmail.com>
2023-06-05 11:29:40 -0600

    initial commit

---
+++ Advanced Programming/Assessment/Integer Sum/integer_sum.py
@@ -0,0 +1,10 @@
+"""
+Integer Sum
+
+Write a function named integer_sum that accepts any number of positional arguments,
+which are assumed to be integers. This function should return the sum of all these
+integers.
+
+To handle invalid input (arguments that are not integers) you must write the
+following decorators
+"""
